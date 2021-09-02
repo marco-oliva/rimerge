3e74795d51bb429725d5017967674153892fb102
Marco Oliva <marco.oliva@ufl.edu>
2021-09-02 17:45:29 +0200

    ignore

--- src/r-index-rle.cpp
+++ src/r-index-rle.cpp
@@ -449,6 +449,7 @@ buildRA(const RIndex<RIndexRLE, RLEString>& left, const RIndex<RIndexRLE, RLEStr
 {
     double start = readTimer();
     
+    omp_set_num_threads(buffers.parameters.merge_jobs);
     #pragma omp parallel for schedule(dynamic, buffers.parameters.chunk_size)
     for (size_type sequence = 0; sequence < right.sequences(); sequence++)
     {
